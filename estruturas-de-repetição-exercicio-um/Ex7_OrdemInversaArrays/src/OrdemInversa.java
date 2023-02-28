04a8ab1a3b289db26e69a727e3daebe2649fcad5
Gustavo-Barbos <gustavo.soares.gsb@gmail.com>
2023-02-28 19:54:40 -0300

    Exemplo 7. Mudar a ordem de um vetor

---
+++ estruturas-de-repetição-exercicio-um/Ex7_OrdemInversaArrays/src/OrdemInversa.java
@@ -0,0 +1,11 @@
+public class OrdemInversa {
+    public static void main(String[] args) throws Exception {
+        int[] vetor = {-5, -11, 40, -30, 67 ,81};
+
+        for(int i = (vetor.length - 1); i >= 0; i--){
+            System.out.println(vetor[i]);            
+
+        }
+    
+    }
+}
